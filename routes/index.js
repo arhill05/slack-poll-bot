3232fa39686fefc07aa651690ac3fd8ccc91ef1d
Drew Hill <andrew.hill@e-farmcredit.com>
2019-09-20 08:15:57 -0400

    add response for user interactions

--- routes/index.js
+++ routes/index.js
@@ -1,68 +1,86 @@
 var express = require('express');
 var router = express.Router();
 const numWords = require('num-words');
+const axios = require('axios');
 
 /* GET home page. */
-router.get('/', function (req, res, next) {
+router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
 router.post('/events/polls', (req, res, next) => {
-  res.status(200).send(req.body.challenge)
-})
+  res.status(200).send(req.body.challenge);
+});
 
 router.post('/events/polls/create-poll', (req, res, next) => {
   const text = req.body.text;
-  const entries = text.split('"').map(x => x.trim()).filter(x => x !== "");
+  const entries = text
+    .split('"')
+    .map(x => x.trim())
+    .filter(x => x !== '');
   const question = entries.shift();
   const formattedPollText = getFormattedPollText(question, entries);
   const actions = getActions(entries);
   const response = {
-    response_type: "in_channel",
+    response_type: 'in_channel',
     as_user: true,
     blocks: [
       {
-        type: "section",
+        type: 'section',
         text: {
-          type: "mrkdwn",
+          type: 'mrkdwn',
           text: formattedPollText
         }
       },
       actions
-    ],
-  }
+    ]
+  };
   res.status(200).send(response);
 });
 
-router.post('/events/polls/user-interaction', (req, res, next) => {
-  console.log(req.body);
+router.post('/events/polls/user-interaction', async (req, res, next) => {
+  const payload = req.body.payload;
+
   res.status(200).send();
-})
+  await sendInteractiveResponse(payload);
+});
+
+sendInteractiveResponse = async payload => {
+  const responseUrl = payload.responseUrl;
+  const response = {
+    replaceOriginal: true,
+    text: (payload.message.text += payload.actions[0].value)
+  };
+
+  await axios.post(responseUrl, response);
+};
 
 getFormattedPollText = (question, entries) => {
-  let result = "";
+  let result = '';
   result += `*${question}*\n\n`;
-  entries.forEach((entry, index) => result += `:${numWords(index + 1)}: ${entry}\n`)
+  entries.forEach(
+    (entry, index) => (result += `:${numWords(index + 1)}: ${entry}\n`)
+  );
   return result;
-}
+};
 
-getActions = (entries) => {
+getActions = entries => {
   const elements = entries.map((entry, index) => ({
-    type: "button",
+    type: 'button',
     text: {
-      type: "plain_text",
+      type: 'plain_text',
       emoji: true,
-      text: `:${numWords(index + 1)}:`,
+      text: `:${numWords(index + 1)}:`
     },
     value: entry
   }));
 
   const actionsBlock = {
-    type: "actions",
+    type: 'actions',
     elements
   };
 
   return actionsBlock;
-}
+};
 
 module.exports = router;
